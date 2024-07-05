655a23b19f36b89dbc2cbdab0802f64c53814595
rem-lover <moseswong1010@gmail.com>
2024-07-05 18:13:39 +0800

    turn func

--- cube.py
+++ cube.py
@@ -37,12 +37,12 @@ class Cube:
     # Returns a 5x5x5 matrix where the cube is solved
     def solved_state(self) -> np.ndarray:
         state = np.full((5, 5, 5), None)
-        state[0, 1:4, 1:4] = COLOURS[0]
-        state[4, 1:4, 1:4] = COLOURS[1]
-        state[1:4, 1:4, 4] = COLOURS[2]
-        state[1:4, 1:4, 0] = COLOURS[3]
-        state[1:4, 4, 1:4] = COLOURS[4]
-        state[1:4, 0, 1:4] = COLOURS[5]
+        state[0, 1:4, 1:4] = COLOURS[0]  # up
+        state[4, 1:4, 1:4] = COLOURS[1]  # down
+        state[1:4, 1:4, 4] = COLOURS[2]  # right
+        state[1:4, 1:4, 0] = COLOURS[3]  # left
+        state[1:4, 4, 1:4] = COLOURS[4]  # front
+        state[1:4, 0, 1:4] = COLOURS[5]  # back
         return state
 
     # Returns True if cube is solved
@@ -75,14 +75,46 @@ class Cube:
         print(" " * (d + 1), down[0], "\n", " " * d, down[1], "\n", " " * d, down[2])
         print(" ")
 
+    def turn(self, actions):
+        """
+        actions: U D R L F B U_p D_p R_p L_p F_p B_p
+        """
+        match actions:
+            case "U":
+                self.state[:2] = np.rot90(self.state[:2], axes=(2, 1))
+            case "U_p":
+                self.state[:2] = np.rot90(self.state[:2], axes=(1, 2))
+            case "D":
+                self.state[3:] = np.rot90(self.state[3:], axes=(1, 2))
+            case "D_p":
+                self.state[3:] = np.rot90(self.state[3:], axes=(2, 1))
+            case "R":
+                self.state[:, :, 3:] = np.rot90(self.state[:, :, 3:], axes=(0, 1))
+            case "R_p":
+                self.state[:, :, 3:] = np.rot90(self.state[:, :, 3:], axes=(1, 0))
+            case "L":
+                self.state[:, :, :2] = np.rot90(self.state[:, :, :2], axes=(1, 0))
+            case "L_p":
+                self.state[:, :, :2] = np.rot90(self.state[:, :, :2], axes=(0, 1))
+            case "F":
+                self.state[:, 3:] = np.rot90(self.state[:, 3:], axes=(2, 0))
+            case "F_p":
+                self.state[:, 3:] = np.rot90(self.state[:, 3:], axes=(0, 2))
+            case "B":
+                self.state[:, :2] = np.rot90(self.state[:, :2], axes=(0, 2))
+            case "B_p":
+                self.state[:, :2] = np.rot90(self.state[:, :2], axes=(2, 0))
+
 
 cube = Cube(
     input_string="y1 y2 y3 y4 y5 y6 y7 y8 y9 w1 w2 w3 w4 w5 w6 w7 w8 w9 r1 r2 r3 r4 r5 r6 r7 r8 r9 o1 o2 o3 o4 o5 o6 o7 o8 o9 b1 b2 b3 b4 b5 b6 b7 b8 b9 g1 g2 g3 g4 g5 g6 g7 g8 g9"
 )
 
-cube.print_state(d=15)
-# cube.turn("R")
-# cube.print_state()
+# cube.print_state(d=15)
+cube.turn("B_p")
+print(cube.state)
+# cube.print_state(d=15)
+
 
 # print(cube.is_solved())
 # cube = Cube()
